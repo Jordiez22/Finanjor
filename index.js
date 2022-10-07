d6160efed86597d46b3af86545d59fe17b9dd19c
Jorge Soto <junaa@gmail.com>
2022-10-07 08:47:24 -0500

    add modified:   index.html add	modified:   index.js add modified:   style.css

--- index.js
+++ index.js
@@ -1,33 +1,73 @@
-function gen_table(){
-    document.getElementById("tab").innerHTML="";
-    let n=Number(document.getElementById("capital").value);
-    let n2=Number(document.getElementById("couta").value);
-    let n3=Number(document.getElementById("interes").value);
-    if(n>0){   
-        for(i=1;i<=n2;i++){
-            ca=n/n2;
-            d1=ca.toFixed(2);
-            i2=((n*n3)/100)/n2;
-            d2=i2.toFixed(2);
-            r=ca+i2;
-            d3=r.toFixed(2);
-            document.getElementById("tab").innerHTML=document.getElementById("tab").innerHTML+
-                    `<tr>
-                        <td> ${i}</td>
-                        <td> ${d1}</td>
-                        <td> ${d2}</td>
-                        <td> ${d3}</td>
-                    </tr>`;
-        }
-        n1=n.toFixed(2);
-        t_i=i2*n2;
-        d4=t_i.toFixed(2);
-        t_p=r*n2;
-        d5=t_p.toFixed(2);
-        document.getElementById("t1").innerHTML=n1;
-        document.getElementById("t2").innerHTML=d4;
-        document.getElementById("t3").innerHTML=d5;        
-    }else{
-        alert("Falta ingresar un Número");
+// Calculate Results
+function calcularResultado() {
+    // UI Vars
+    const monto = document.getElementById("monto");
+    const interes = document.getElementById("interes");
+    const anios = document.getElementById("anios");
+    const pagoMensual = document.getElementById("pago-mensual");
+    const pagoTotal = document.getElementById("pago-total");
+    const totalInteres = document.getElementById("total-interes");
+    
+    const principal = parseFloat(monto.value);
+    const calculatedInteres = parseFloat(interes.value) / 100 / 12;
+    const calculatarPagos = parseFloat(anios.value) * 12;
+  
+    // Computed Monthly payment
+    const x = Math.pow(1 + calculatedInteres, calculatarPagos);
+    const mensual = (principal * x * calculatedInteres) / (x - 1);
+  
+    if (isFinite(mensual)) {
+      pagoMensual.value = mensual.toFixed(2);
+      pagoTotal.value = (mensual * calculatarPagos).toFixed(2);
+      totalInteres.value = (mensual * calculatarPagos - principal).toFixed(2);
+  
+      // Show Results
+      document.getElementById("resultado").style.display = "block";
+  
+      // Hide Loader
+      document.getElementById("cargando").style.display = "none";
+    } else {
+      showError("Por favor compruebe las entradas numéricas.");
     }
-}
+  }
+  
+  //
+  function showError(error) {
+    //
+    document.getElementById("resultado").style.display = "none";
+  
+    //
+    document.getElementById("cargando").style.display = "none";
+  
+    //
+    const errorDiv = document.createElement("div");
+  
+    //
+    const card = document.querySelector(".card");
+    const heading = document.querySelector(".heading");
+  
+    //
+    errorDiv.className = "alert alert-danger";
+  
+    //
+    errorDiv.appendChild(document.createTextNode(error));
+  
+    //
+    card.insertBefore(errorDiv, heading);
+  
+    //
+    setTimeout(clearError, 3000);
+  
+    //
+    function clearError() {
+      document.querySelector(".alert").remove();
+    }
+  }
+  
+  function BotonDePrestamo(){
+    document.getElementById("Solicita-tu-prestamo").style.background = "#4C60EC";
+    document.getElementById("BotonDePrestamo").innerHTML = "¡Listo! en breve recibiras un mail";
+    document.getElementById("BotonDePrestamo").style.background = "MediumSeaGreen";
+   
+    
+  }
